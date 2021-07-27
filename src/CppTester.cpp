8ac54480472be2189bb4c55877ca45d6c46cfacd
ApourtArtt <47370292+ApourtArtt@users.noreply.github.com>
2021-07-27 14:53:29 +0200

    Update CppTester.cpp

--- src/CppTester.cpp
+++ src/CppTester.cpp
@@ -9,7 +9,7 @@ struct testRet
 {
     std::vector<std::tuple<std::string, bool, std::string>> statements;
     void AddStatement(std::string Title, bool Status, std::string location)
-        { statements.push_back({Title, Status, location}); }
+        { statements.push_back({std::move(Title), std::move(Status), std::move(location)}); }
 };
 
 struct benchRet
@@ -49,24 +49,15 @@ struct registrar {
 #define BENCHER(name, message, func) registrar benchR(name, message, []()->benchRet{benchRet r; func return r;});
 #define BENCH(...) BENCHER(__VA_ARGS__)
 
-/*
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-*/
 #define START_BENCH \
     auto t1 = std::chrono::high_resolution_clock::now(); \
     auto t2 = std::chrono::high_resolution_clock::now(); \
     while (duration_cast<std::chrono::milliseconds>(t2-t1).count() < 1000) {
 
-
 #define STOP_BENCH \
         t2 = std::chrono::high_resolution_clock::now(); \
         r.counter++; \
     }
-
-    
 
 #define GET_LOCATION __FILE__ + std::string(":") + std::to_string(__LINE__)
 
@@ -84,6 +75,10 @@ namespace Tester
         if (hasRan) return;
         hasRan = true;
 
+        int nbTestFailed = 0, nbTestPassed = 0;
+
+        std::cout << "Starting tests..." << std::endl;
+
         for (auto& fn: registrar::testers)
         {
             std::cout << "Starting to test: " << fn.title << " - " << fn.subTitle << std::endl;
@@ -92,13 +87,23 @@ namespace Tester
             {
                 auto [subTitle, status, location] = test;
                 if (status)
+                {
                     std::cout << "\t" << subTitle << ": Passed" << std::endl;
+                    nbTestPassed++;
+                }
                 else
+                {
                     std::cout << "\t" << subTitle << ": Failed (" << location << ")" << std::endl;
+                    nbTestFailed++;
+                }
             }
         }
 
-        std::cout << std::endl;
+        std::cout << "Tests executed : " << nbTestFailed + nbTestPassed
+        << ", Passed: " << nbTestPassed << ", Failed: " << nbTestFailed
+        << ", Ignored: " << registrar::testers.size() << std::endl; // TODO: it works (that's strange) on GCC, check for MSVC
+        
+        std::cout << "Starting benchs..." << std::endl;
 
         for (auto& fn: registrar::benchers)
         {
@@ -106,6 +111,8 @@ namespace Tester
             benchRet r = fn.func();
             std::cout << r.counter << " iteration/s" << std::endl;
         }
+
+        std::cout << "Benchs executed\nEvery tasks are finished" << std::endl;
     }
 }
 
@@ -115,15 +122,15 @@ int Bidule(int a, int b)
 }
 
 TEST("BiduleTest", "Tests some additions",
-    EXPECT("Is 6+7 equal to 12", Bidule(6, 7)==12) // false
-    ASSERT("Is 6+6 equal to 12", Bidule(6, 6)==12) // true
-    ASSERT("Is 6+7 equal to 12", Bidule(6, 7)==12) // false, will stop there
-    EXPECT("Is 6+8 equal to 14", Bidule(6, 8)==14) // true
+    EXPECT("Is 6+7 equal to 12", Bidule(6, 7) == 12) // failed
+    ASSERT("Is 6+6 equal to 12", Bidule(6, 6) == 12) // passed
+    ASSERT("Is 6+7 equal to 12", Bidule(6, 7) == 12) // failed, will stop there
+    EXPECT("Is 6+8 equal to 14", Bidule(6, 8) == 14) // ignored
 )
 
 BENCH("BiduleBench", "Bench additions",
-    START_BENCH
     int a = 5; int b = 7;
+    START_BENCH
     Bidule(a, b);
     STOP_BENCH
 )
